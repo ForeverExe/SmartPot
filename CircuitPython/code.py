4ef5126293f97e7bc5f136f519f4f2b6deefb2d7
ForeverExe <matteo.besutti@virgilio.it>
2026-01-15 16:30:51 +0100

    FIXATO, NON USARE QOS2 IN PUBLISH

--- CircuitPython/code.py
+++ CircuitPython/code.py
@@ -30,9 +30,9 @@ print(f"UUID using the processor hex'd UID: {uuid}")
 basic_topic = getenv("MQTT_BASIC_TOPIC")+'/'+uuid #--> /device/<uuid>
 print(f"Basic topic of the device: {basic_topic}")
 
-subscribe_feed = basic_topic+getenv("MQTT_DEVICE_SETTINGS_TOPIC")
+subscribe_feed = basic_topic+getenv("MQTT_DEVICE_SETTINGS_TOPIC") #--> /device/<uuid>/settings
 tel_topic = basic_topic+getenv("MQTT_BASE_TELEMETRY") #--> /device/<uuid>/telemetry
-info_topic = basic_topic+getenv("MQTT_DEVICE_INFO_TOPIC")
+info_topic = basic_topic+getenv("MQTT_DEVICE_INFO_TOPIC") #--> /device/<uuid>/info
 
 device_info ={'name':'SmartPot Proto1','id':uuid,'version':'0.0.01','type':'Raspberry Pi Pico 2W'}
 json_info = json.dumps(device_info, separators=(',',":"))
@@ -44,9 +44,10 @@ record = senML()
 
 ### MQTT Client methods
 def connected(client, userdata, flags, rc):
-    print("Done!")
-    mqtt_client.publish(info_topic, json_info, True, 2)
-    
+    print("Done! Publishing the device informations...")
+    print(str(rc)+"\n\n")
+    mqtt_client.publish(info_topic, json_info, True, 1)
+
 def disconnected(client, userdata, rc):
     print("Disconnected from the Broker")
     
@@ -55,6 +56,7 @@ def message(client, topic, message):
 
 def apply_settings_msg(client, topic, message):
     print("ooooh ho ricevuto qualcosa sul topic dei settings!")
+    print(str(message))
 
 #### Pot Methods
 def update_and_send():
@@ -66,47 +68,57 @@ def update_and_send():
 #### MAIN
 
 print(settings)
-print(f"Topics:\nSettings: {subscribe_feed} - Telemetry: {tel_topic} - Device Info: {info_topic}")
-
+print(f"\nTopics:\nSettings: {subscribe_feed} - Telemetry: {tel_topic} - Device Info: {info_topic}")
 
 wifi.radio.connect(ssid, password)
 print("Connected to Wifi!")
 print("My IP addr:", wifi.radio.ipv4_address)
-   
+
 pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
 ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
 
+#mqtt_client = MQTT.MQTT(
+#    broker = mqtt_ip,
+#    port = mqtt_port,
+#    username = mqtt_username,
+#    password = mqtt_password,
+#    client_id = 'test1', #"Pot"+uuid
+#    socket_pool = pool,
+#    ssl_context = ssl_context,
+#    use_binary_mode = False,
+#)
+
 mqtt_client = MQTT.MQTT(
     broker = mqtt_ip,
-    port = mqtt_port,
+    port = int(mqtt_port),
     username = mqtt_username,
     password = mqtt_password,
-    client_id=uuid,
     socket_pool = pool,
-    ssl_context = ssl_context
+    ssl_context = ssl_context,
 )
+
 logger = mqtt_client.enable_logger(adafruit_logging, adafruit_logging.DEBUG, "loggy")
 
 mqtt_client.on_connect = connected
 mqtt_client.on_disconnect = disconnected
 mqtt_client.add_topic_callback(subscribe_feed, apply_settings_msg)
 print("Connecting to the broker...")
-mqtt_client.connect(True)
+mqtt_client.connect()
 
-if(mqtt_client.is_connected()):
-    print("Connected to the Broker, sending device information...")
-    mqtt_client.publish(info_topic, json_info, True, 2)
-else:
-    print("Connection Error: make sure the credentials are correct.")
+#if(mqtt_client.is_connected()):
+#    print("Connected to the Broker, sending device information...")
+#    mqtt_client.publish(info_topic, "vfjbvfeve", True, 2)
+#    time.sleep(1)
+#else:
+#    print("Connection Error: make sure the credentials are correct.")
     
 print("Connection Established: subscribing and listening to the settings topic...")
-#mqtt_client.subscribe(subscribe_feed, 2)
+mqtt_client.subscribe(subscribe_feed, 2)
 
 while True:
     try:
         mqtt_client.loop(5)
-        mqtt_client.publish(info_topic, json_info, True, 2)
-        #update_and_send()
+        update_and_send()
     except (ValueError, RuntimeError) as e:
         print("Failed to get data, retrying\n", e)
         wifi.reset()
