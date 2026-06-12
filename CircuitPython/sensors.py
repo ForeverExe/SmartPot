5beae28f86b89a8908d41626f295781530212e39
ForeverExe <matteo.besutti@virgilio.it>
2026-06-12 17:50:36 +0200

    da implementare la logica su core, poi rifare circuito

--- CircuitPython/sensors.py
+++ CircuitPython/sensors.py
@@ -1,45 +1,34 @@
-import analogio
-import digitalio
-from board import *
 import board
-import microcontroller
+import time
 import adafruit_dht
+from analogio import AnalogIn
+from digitalio import DigitalInOut, Direction, Pull
 
-soilhum:analogio.AnalogIn
-airhum:analogio.AnalogIn
-ledhose:digitalio.DigitalInOut
+DHT_PIN = board.GP14
+LED_PIN = board.LED #board.GP13
+SOIL_PIN = board.GP28_A2
+LED_PIN = board.GP5
 
-#sh_power=digitalio.DigitalInOut(board.GP27)
-#sh_power.direction = digitalio.Direction.OUTPUT
-#sh_power.value=True
-#soilhum = analogio.AnalogIn(board.GP26_A0)
+dht = adafruit_dht.DHT11(DHT_PIN)
+soil_device = AnalogIn(SOIL_PIN)
+led = DigitalInOut(LED_PIN)
+led.direction = Direction.OUTPUT
 
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
 
-#led = digitalio.DigitalInOut(board.LED)
-#led.direction = digitalio.Direction.OUTPUT
-#led.value=True
-
-#while(True):
-    #temp = (soilhum.value * 222.2) - 61.111 #necessita dei 5v
-    #print(f"Temperature: {temp}")
-    #print(f"Soilhum: {soilhum.value}")
-    #print(f"Led: {led.value}")
-    #led.value = not led.value
-    #time.sleep(1)
-
-def init_soilhum(gpio:microcontroller.Pin):
-    soilhum = analogio.AnalogIn(gpio)
-    
-def init_airhum(gpio:microcontroller.Pin):
-    airhum = adafruit_dht.DHT11(gpio)
-    
-def init_waterled(gpio:microcontroller.Pin):
-    ledhose = digitalio.DigitalInOut(gpio)
-    ledhose.direction = digitalio.Direction.OUTPUT
-
-
-def read_soil_hum():
-    return temp = (soilhum.value * 222.2) - 61.111 #necessita dei 5v
-
-def read_air_hum():
-    
+while True:
+    try:
+        temperature = dht.temperature
+        humidity = dht.humidity
+        soil_hum = soil_device.value
+        print(f"DHT: Temp: {temperature} - Air Humidity: {humidity}")
+        print(f"Capacitive Soil Humidity: {soil_hum}")
+        if(soil_hum < 2):
+            led.value(True)
+            time.sleep(5)
+            led.value(False)
+        time.sleep(1)
+    except RuntimeError as e:
+        # Reading doesn't always work! Just print error and we'll try again
+        print("Reading from DHT failure: ", e.args)
